c10c2f83d99ce7c21d5eb79323bf534634c7f398
Gabriel Cardoso de Castro <94929702+GabrielCCastrodev@users.noreply.github.com>
2024-12-17 19:58:51 -0300

    Enviando comando de agoar para o nodeMcu

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -132,7 +132,18 @@ void messageReceived(char* topic, byte* payload, unsigned int length) {
       if (IPNodeMcu == "DISPOSITIVO_NAO_ENCONTRADO") {
         Serial.println("Dispositivo não encontrado com o Identificador: " + String(identifier));
       } else {
-        Serial.println("IP do NodeMCU encontrado: " + IPNodeMcu);
+          DynamicJsonDocument doc(1024);
+          doc["comand"] = comand;
+          doc["time"] = time;
+
+          String jsonString;
+          serializeJson(doc, jsonString);
+
+          udp.beginPacket(IPNodeMcu.c_str(), porta);
+          udp.write((const uint8_t*)jsonString.c_str(), jsonString.length());
+          udp.endPacket();
+
+          Serial.println("Comando enviado para o NodeMCU: " + jsonString);
       }
 
     } else {
@@ -150,15 +161,14 @@ void connectAWS() {
   secureClient.setPrivateKey(privatePemKey);
 
   mqttClient.setServer(awsEndpoint, awsPort);
-  mqttClient.setCallback(messageReceived); // Configura o callback
+  mqttClient.setCallback(messageReceived);
 
-  // Tentando a conexão
   while (!mqttClient.connected()) {
     Serial.println("Conectando ao AWS IoT Core...");
-    String clientID = "irrigation-gateway-000";  // Usando um clientID único
+    String clientID = "irrigation-gateway-000";
     if (mqttClient.connect(clientID.c_str())) {
       Serial.println("Conectado ao AWS IoT Core!");
-      // Inscreve-se no tópico após a conexão
+
       if (mqttClient.subscribe("irrigation/control/000/command")) {
         Serial.println("Inscrito no tópico: irrigation/control/000/command");
       } else {
@@ -167,7 +177,7 @@ void connectAWS() {
     } else {
       Serial.print("Falha na conexão. Estado MQTT: ");
       Serial.println(mqttClient.state());
-      delay(2000); // Aguarde antes de tentar novamente
+      delay(2000);
     }
   }
 }
@@ -178,7 +188,6 @@ void setup() {
 
   pinMode(botaoPinSolicitarDados, INPUT_PULLUP);
 
-  // Conectando-se à rede Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -186,27 +195,21 @@ void setup() {
   }
   Serial.println("Conectado à rede Wi-Fi!");
 
-  // Inicia o servidor UDP
   udp.begin(porta);
 
-  // Calcula o endereço de broadcast
   broadcastIP = WiFi.localIP();
-  broadcastIP[3] = 255; // Broadcast na última parte do IP da rede
+  broadcastIP[3] = 255;
 
-  // Cria o array de respostas no JSON
   respostaJson.createNestedArray("respostas");
 
-  // Conecta ao AWS IoT Core
   connectAWS();
 }
 
 void loop() {
-  // Verifica se o botão foi pressionado
   if (digitalRead(botaoPinSolicitarDados) == LOW && !botaoPressionado) {
     botaoPressionado = true;
     tempoInicial = millis();
 
-    // Limpa as respostas anteriores no JSON
     respostaJson["respostas"].clear();
 
     Serial.print("Botão apertado");
@@ -228,10 +231,8 @@ void loop() {
         incomingPacket[len] = 0; // Garante que a string seja terminada corretamente
       }
 
-      // Serializa a resposta recebida no JSON
-      String resposta = String(incomingPacket); // Converte para String
+      String resposta = String(incomingPacket);
 
-      // Adiciona a resposta como um item no array de "respostas"
       respostaJson["respostas"].add(resposta);
 
       Serial.print("Resposta recebida: ");
@@ -241,23 +242,20 @@ void loop() {
     botaoPressionado = false;
     Serial.println("Tempo de coleta de respostas encerrado.");
 
-    // Processa e formata as respostas recebidas
     JsonArray respostas = respostaJson["respostas"].as<JsonArray>();
     DynamicJsonDocument jsonFormatado(2048);
-    JsonArray respostasFormatadas = jsonFormatado.to<JsonArray>(); // Ajuste aqui: cria um array diretamente no JSON raiz
+    JsonArray respostasFormatadas = jsonFormatado.to<JsonArray>();
 
     for (JsonVariant resposta : respostas) {
         String dado = resposta.as<String>();
 
-        // Variáveis temporárias para armazenar os valores extraídos
         char identificador[16];
         float temperatura, umidade, umidade_solo, temperatura_solo;
         int intensidade_uv, intensidade_luz;
 
-        // Extração dos valores com sscanf
         if (sscanf(dado.c_str(), "Identificador: %[^,], Temperatura: %f, Umidade: %f, Umidade_Solo: %f, Intensidade_Luz: %d, Intensidade_UV: %d, Temperatura_Solo: %f",
                    identificador, &temperatura, &umidade, &umidade_solo, &intensidade_luz, &intensidade_uv, &temperatura_solo) == 7) {
-            // Cria um objeto JSON para cada resposta formatada
+
             JsonObject item = respostasFormatadas.createNestedObject();
             item["identifier"] = identificador;
             JsonObject valores = item.createNestedObject("values");
@@ -272,16 +270,14 @@ void loop() {
 
     if (mqttClient.state() != MQTT_CONNECTED) {
         Serial.println("Conexão MQTT perdida. Tentando reconectar...");
-        connectAWS();  // Reconectar
+        connectAWS();
     }
 
-    // Converte o JSON para string para envio
     String jsonString;
     serializeJson(jsonFormatado, jsonString);
     Serial.println("Respostas formatadas em JSON:");
     Serial.println(jsonString);
 
-    // Publica o JSON no tópico MQTT
     if (!mqttClient.publish(publishTopic, jsonString.c_str())) {
         Serial.print("Falha ao publicar no tópico MQTT. Estado MQTT: ");
         Serial.println(mqttClient.state());
@@ -294,7 +290,6 @@ void loop() {
     }
   }
 
-  // Mantém a conexão MQTT
   mqttClient.loop();
   delay(10);
 }
